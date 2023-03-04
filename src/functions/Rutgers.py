9ff7104bcf97979487e51a0c2993008b1567816c
itareque <93095686+itareque@users.noreply.github.com>
2023-03-04 15:55:55 -0500

    Add spaces to the code

--- src/functions/Rutgers.py
+++ src/functions/Rutgers.py
@@ -62,31 +62,31 @@ class Dispose:
         self.data_frame_obj = pd.DataFrame
         self.pandas_frame_data = pd.DataFrame()
 
-    def get_file_directory(self,path,suffix = None, exclude = None,):
+    def get_file_directory(self, path, suffix = None, exclude = None,):
 
         suffix = suffix if suffix else '.txt'
         exclude = exclude if exclude else []
 
         file_list = os.listdir(path)
         for file_item in file_list:
-            name,suffix_ = os.path.splitext(file_item)
+            name, suffix_ = os.path.splitext(file_item)
             if suffix_ == suffix:
                 self.file_queue.append(file_item)
 
         # self.file_queue = os.listdir(path)
         self.file_path = path
 
-    def open_file(self,path_file = None,mode = 'r',encoding = 'utf-8'):
+    def open_file(self, path_file = None, mode = 'r', encoding = 'utf-8'):
 
         path = self.file_path
         path_file = path_file if path_file else self.file_queue
         for file_path in path_file:
             file_path_all = rf'{path}\{file_path}'
-            with open(file_path_all,mode,encoding=encoding) as file:
-                name,suffix = os.path.splitext(file_path)
+            with open(file_path_all, mode, encoding = encoding) as file:
+                name, suffix = os.path.splitext(file_path)
                 self.file_data.update({name:file.read()})
 
-    def re_non_exclusionary(self,re_str,key_dict = 'Exclusionary'):
+    def re_non_exclusionary(self, re_str, key_dict = 'Exclusionary'):
 
         re_da_str = ''
         count_str = ''
@@ -98,7 +98,7 @@ class Dispose:
         # key_frequency = None
         key_count = 0
 
-        for key,value in self.disp_data[key_dict].items():
+        for key, value in self.disp_data[key_dict].items():
             re_count = re_str.count(key.lower())
             if re_count:
                 re_success_data.append(key)
@@ -107,17 +107,17 @@ class Dispose:
                 key_count += count_str_len
                 value_frequency.update({key:re_count / count_str_len})
                 re_value = key_dict
-        for key,value in re_data_count.items():
+        for key, value in re_data_count.items():
             re_da_str += f'/{key}' if re_da_str else key
             count_str += f'/{key}:{value}' if count_str else f'{key}:{value}'
-        data_compute = {'len':len_re_str,'key_frequency':float(f'{key_count / len_re_str:.2}') if len_re_str else None,'value_frequency':value_frequency,'re_success_data':re_success_data,'re_data_count':re_data_count}
+        data_compute = {'len':len_re_str, 'key_frequency':float(f'{key_count / len_re_str:.2}') if len_re_str else None,'value_frequency':value_frequency,'re_success_data':re_success_data,'re_data_count':re_data_count}
         re_data = {'High School':None,'Discipline Approach':re_da_str,'Exclusionary/Non-exclusionary':re_value,f'Count':count_str}
-        return re_data,data_compute
+        return re_data, data_compute
 
-    def _dict_str(self,dict):
+    def _dict_str(self, dict):
 
         str_dict = ''
-        for key,value in dict.items():
+        for key, value in dict.items():
             value = f'{value:.2e}'
             if str_dict:
                 str_dict += f'/{key}:{value}'
@@ -125,17 +125,17 @@ class Dispose:
             str_dict += f'{key}:{value}'
         return str_dict
 
-    def re_value(self,re_str,key = None):
+    def re_value(self, re_str, key = None):
 
         excl_mode = None
-        re_dict_a,data_compute_a = self.re_non_exclusionary(re_str)
-        re_dict_b,data_compute_b = self.re_non_exclusionary(re_str,'Non-exclusionary')
+        re_dict_a, data_compute_a = self.re_non_exclusionary(re_str)
+        re_dict_b, data_compute_b = self.re_non_exclusionary(re_str, 'Non-exclusionary')
         excl_freq_a = data_compute_a['key_frequency']
         non_excl_freq_b = data_compute_b['key_frequency']
         excl_value_freq_a = self._dict_str(data_compute_a['value_frequency'])
         non_excl_value_freq_b = self._dict_str(data_compute_b['value_frequency'])
 
-        key = key.replace('_',' ') if type(key) == str else key
+        key = key.replace('_', ' ') if type(key) == str else key
         try:
             if excl_freq_a == non_excl_freq_b:
                 excl_mode = 'Exclusionary/Non-exclusionary'
@@ -152,15 +152,15 @@ class Dispose:
     def process_the_data(self):
 
         for key, value in self.file_data.items():
-            d = self.re_value(value,key)
+            d = self.re_value(value, key)
             self.dict_data.append(d)
 
-    def pandas_file_value(self,):
+    def pandas_file_value(self, ):
         data_frame = pd.DataFrame(self.dict_data)
         self.pandas_frame_data = data_frame
 
 
-def main(txt_file_path,execl_path):
+def main(txt_file_path, execl_path):
     '''
     
     :param txt_file_path:
@@ -173,8 +173,8 @@ def main(txt_file_path,execl_path):
     a.process_the_data()
     a.pandas_file_value()
     print(a.pandas_frame_data)  
-    a.pandas_frame_data.to_excel(execl_path,index=False)
+    a.pandas_frame_data.to_excel(execl_path, index = False)
 
 if __name__ == '__main__':
-    # main('txt_file_path','execl_file_path')
+    # main('txt_file_path', 'execl_file_path')
     main(r'\data.xlsx')
